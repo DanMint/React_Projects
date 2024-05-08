2496ff6390b5669101b91ada2761583d28bf77b3
mints.d <dannymints@gmail.com>
2024-05-07 23:25:57 -0400

    Added the usage of onChnage to React_basics

---
+++ React_Basics/React_onChange/src/App.jsx
@@ -0,0 +1,16 @@
+// onChnage = event handler usualy used with form elements. ex: <input> <textarea>,..... etc
+// it tiggers a function every time the value of the input changes
+
+import MyComponent from "./MyComponent/MyComponent.jsx"
+
+function App() {
+
+  return (
+    <>
+      <MyComponent />
+    </>
+    
+  )
+}
+
+export default App
---
+++ React_Basics/React_onChange/src/MyComponent/MyComponent.jsx
@@ -0,0 +1,67 @@
+import React, {useState} from "react";
+
+function MyComponent() {
+    const [name, setName] = useState("Guest");
+    const [amount, setAmount] = useState(0);
+    const [review, setReview] = useState("")
+    // drop down menue
+    const  [payment, setPayment] = useState("NONE");
+    // radio method
+    const [shipping, setShipping] = useState("");
+
+    function handleNameChange(event) {
+        setName(event.target.value);
+    }
+
+    function handleAmountChange (event) {
+        setAmount(event.target.value);
+    }
+
+    function handleCommentChange(event) {
+        setReview(event.target.value)
+    }
+
+    function handlePaymentChnage(event) {
+        setPayment(event.target.value);
+    }
+
+    function handleShippingChange(event) {
+        setShipping(event.target.value);
+    }
+
+    return(
+        <>
+            <div>
+                <input value={name} onChange={handleNameChange} />
+                <p>Name: {name}</p>
+
+                <input value={amount} onChange={handleAmountChange} type="number" />
+                <p>Amount: {amount}</p>
+
+                <textarea value={review} onChange={handleCommentChange} placeholder="Please leave a review" />
+                <p>Review: {review}</p>
+
+                <select value={payment} onChange={handlePaymentChnage}>
+                    <option value="">Select and option</option>
+                    <option value="Visa">Visa</option>
+                    <option value="Mastercard">Mastercard</option>
+                    <option value="Gift Card">Gift Card</option>
+                </select>
+                <p>payment: {payment}</p>
+
+                <label>
+                    <input type="radio" value="Pick up" checked={shipping === "Pick up"} onChange={handleShippingChange}/> 
+                    Pick up
+                </label>
+
+                <label>
+                <input type="radio" value="Delivery" checked={shipping === "Delivery"} onChange={handleShippingChange}/>
+                    Delivery
+                </label>
+                <p>Shipping: {shipping}</p>
+            </div>
+        </>
+    );
+}
+
+export default MyComponent
