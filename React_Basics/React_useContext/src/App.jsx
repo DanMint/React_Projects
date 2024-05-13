134af6cc7fa08cbdfca01099f342b878732d98ab
mints.d <dannymints@gmail.com>
2024-05-12 23:55:17 -0400

    Understood how to use useContext

---
+++ React_Basics/React_useContext/src/App.jsx
@@ -0,0 +1,14 @@
+// useContext() = its a hook a react that allows to share values between multiple levels of components withoutn passing props through each level 
+
+import ComponentA from "./Components/ComponentA.jsx"
+
+function App() {
+
+  return (
+    <>
+    <ComponentA />
+    </>
+  )
+}
+
+export default App
