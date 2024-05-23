f2a9d0d8cf959572cf0985640af0b177605d9870
Daniel <dannymints@gmail.com>
2024-05-23 00:18:08 -0400

    Countinued work on calculator project. Fixed some logic

--- React_Basic_Projects/Project_calculator/src/Calculator/Calculator.jsx
+++ React_Basic_Projects/Project_calculator/src/Calculator/Calculator.jsx
@@ -3,72 +3,149 @@ import styles from './Calculator.module.css'
 import React, {useState} from 'react';
 
 function Calculator() {
-    const [value, setValue] = useState(0);
-    const [output, setOutput] = useState("");
+    let [value, setValue] = useState(0);
+    let [output, setOutput] = useState("");
 
-    const [valA, setValA] = useState("");
-    const [valB, setValb] = useState("");
-    const [operator, setOperator] = useState('');
-    const [equal, setEqual] = useState(false);
+    let [numbers, setNumbers] = useState(0)
+    let [currentInput, setCurrentInput] = useState("");
+    let [operatorAmount, setOperatorAmount] = useState(0);
+    let [operator, setOperator] = useState(0);
+    let [equal, setEqual] = useState(false);
 
     function putOne() {
-       if (operator == '') {
-        valA += 1;
+       if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 1);
+            setOutput(currentInput);
+            setOutput(output += 1);
        }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 1);
+            setOutput(output += 1);
+       }
+
     }
 
     function putTwo() {
-        console.log(value);
-        setValue(1);
-        console.log(value);
+        if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 2);
+            setOutput(currentInput);
+            setOutput(output += 2);
+       }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 2);
+            setOutput(output += 2);
+       }
     }
 
     function putThree() {
-        console.log(value);
-        setValue(1);
-        console.log(value);
+        if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 3);
+            setOutput(currentInput);
+            setOutput(output += 3);
+       }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 3);
+            setOutput(output += 3);
+       }
     }
 
     function putFour() {
-        console.log(value);
-        setValue(1);
-        console.log(value);
+        if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 4);
+            setOutput(currentInput);
+            setOutput(output += 4);
+       }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 4);
+            setOutput(output += 4);
+       }
     }
 
     function putFive() {
-        console.log(value);
-        setValue(1);
-        console.log(value);
+        if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 5);
+            setOutput(currentInput);
+            setOutput(output += 5);
+       }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 5);
+            setOutput(output += 5);
+       }
     }
 
     function putSix() {
-        console.log(value);
-        setValue(1);
-        console.log(value);
+        if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 6);
+            setOutput(currentInput);
+            setOutput(output += 6);
+       }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 6);
+            setOutput(output += 6);
+       }
     }
 
     function putSeven() {
-        console.log(value);
-        setValue(1);
-        console.log(value);
+        if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 7);
+            setOutput(currentInput);
+            setOutput(output += 7);
+       }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 7);
+            setOutput(output += 7);
+       }
     }
 
     function putEight() {
-        console.log(value);
-        setValue(1);
-        console.log(value);
+        if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 8);
+            setOutput(currentInput);
+            setOutput(output += 8);
+       }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 8);
+            setOutput(output += 8);
+       }
     }
 
     function putNine() {
-        console.log(value);
-        setValue(1);
-        console.log(value);
+        if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 9);
+            setOutput(currentInput);
+            setOutput(output += 9);
+       }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 9);
+            setOutput(output += 9);
+       }
     }
 
     function putZero() {
-        console.log(value);
-        setValue(1);
-        console.log(value);
+        if (numbers == 0 && operatorAmount == 0) {
+            setCurrentInput(currentInput += 0);
+            setOutput(currentInput);
+            setOutput(output += 0);
+       }
+       
+       else if (numbers == operatorAmount) {
+            setCurrentInput(currentInput += 0);
+            setOutput(output += 0);
+       }
+    }
+
+    function addPlus() {
+            setOperators(operators.push('+'))
+
     }
 
     return(
@@ -79,22 +156,22 @@ function Calculator() {
                 </div>
                 <div className='styles.buttonsNumbers'>
                     <button onClick={putOne}>1</button>
-                    <button>2</button>
-                    <button>3</button>
+                    <button onClick={putTwo}>2</button>
+                    <button onClick={putThree}>3</button>
                     <br></br>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
+                    <button onClick={putFour}>4</button>
+                    <button onClick={putFive}>5</button>
+                    <button onClick={putSix}>6</button>
                     <br></br>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
+                    <button onClick={putSeven}>7</button>
+                    <button onClick={putEight}>8</button>
+                    <button onClick={putNine}>9</button>
                     <br />
-                    <button>0</button>
+                    <button onClick={putZero}>0</button>
                 </div>
 
                 <div className='styles.arithmeticOperators'>
-                    <button>+</button>
+                    <button onClick={addPlus}>+</button>
                     <button>-</button>
                     <button>*</button>
                     <button>/</button>
